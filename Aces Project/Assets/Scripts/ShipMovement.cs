3eb4953434f65fba0f0ff74b574ccddf6a67b552
Connor (Home) <connorcoxr@gmail.com>
2020-10-30 02:41:19 -0600

    Space Sky Sphere shader created

---
+++ Aces Project/Assets/Scripts/ShipMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.UIElements;
+
+public class ShipMovement : MonoBehaviour
+{
+
+    public Rigidbody controllerBody;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if()
+    }
+}
