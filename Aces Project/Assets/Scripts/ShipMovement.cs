4188b138c93572557610c157715c0f45c08b83dc
Connor (Home) <connorcoxr@gmail.com>
2020-11-03 13:04:43 -0700

    Movement updates, camera now adjustable
    Former-commit-id: e02ec5ddfa683491186f401d5f07a181f8634840 [formerly d17ea7c2c006871dc25b37b1b6bd259178183fe7] [formerly 4fb60760d123bd0fe6b0861522f79f0c2f8d306a [formerly 5b1c4d49759a005a6d265ac86b7b13fe13c64363]]
    Former-commit-id: f6670f6ca4a6b91a0af827226d9ae4ef32928708 [formerly 79b0426a0e7c96c01c46091bab0b97d6b5be688f]
    Former-commit-id: 84842f19e9e27f28daf674a0bda8099ffddf8b93

--- Aces Project/Assets/Scripts/ShipMovement.cs
+++ Aces Project/Assets/Scripts/ShipMovement.cs
@@ -1,4 +1,6 @@
 ﻿using System.Security.AccessControl;
+using System.Threading;
+using Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,96 +9,122 @@ public class ShipMovement : MonoBehaviour
 {
 
     public float thrust = 1,
-        maxSpeed = 10,
+        minSpeed = 5,
+        maxSpeed = 50,
         yawSpeed = 0.1f,
         upSpeed = 5,
         downSpeed = 3,
         breakForce = 1.1f,
         twistSpeed = 1,
-        highSpeedTurning = 6;
+        highSpeedTurning = 6,
+        accelerationRate = 5;
 
-    public Vector3 _bodyVelocity;
+    public CinemachineVirtualCamera camera;
+
+    private Vector3 _bodyVelocity;
 
     private Rigidbody _controllerBody;
-    private Gamepad _gamepad;
+    private Gamepad _g;
     
-    
-    // Start is called before the first frame update
     void Start()
     {
        _controllerBody =  GetComponent<Rigidbody>();
        CheckGamepad();
-       _gamepad = Gamepad.current;
+       _g = Gamepad.current;
     }
 
     private void CheckGamepad()
     {
+        
         if (Gamepad.current == null)
         {
             Debug.Log("no gamepad found");
             return;
         }
-        else
-        {
-            _gamepad = Gamepad.current;
-        }
+        
+        _g = Gamepad.current;
+        
     }
 
-    // Update is called once per frame
     private void FixedUpdate()
     {
+        if (Gamepad.current == null)
+        {
+            Debug.Log("no gamepad found");
+            CheckGamepad();
+        }
         _controllerBody.AddRelativeForce(0,0, thrust);
 
         //accelerate
-        if (_gamepad.rightTrigger.ReadValue() > .2f && thrust < maxSpeed)
+        if (_g.rightTrigger.ReadValue() > .2f && thrust < maxSpeed && !_g.leftTrigger.isPressed)
         {
-            thrust = (_gamepad.rightTrigger.ReadValue() * maxSpeed);
+            thrust = thrust + (_g.rightTrigger.ReadValue() * accelerationRate) * Time.deltaTime;
             _controllerBody.AddRelativeForce(0,0, thrust);
             
             _bodyVelocity = _controllerBody.velocity;
+            if (thrust > maxSpeed)
+            {
+                thrust = maxSpeed;
+            }
             
-            _gamepad.SetMotorSpeeds(_gamepad.rightTrigger.ReadValue()-.65f, _gamepad.rightTrigger.ReadValue()-.35f);
+            _g.SetMotorSpeeds(_g.rightTrigger.ReadValue()-.65f, _g.rightTrigger.ReadValue()-.35f);
         }
-        else if(thrust > 10)
+        else if (thrust > maxSpeed - 5)
         {
-            thrust = Mathf.Lerp(thrust, maxSpeed - 1, (Time.deltaTime/15));
-            _gamepad.SetMotorSpeeds(0, 0);
+            thrust = Mathf.Lerp(thrust, maxSpeed - 5, (Time.deltaTime/15));
+            _g.SetMotorSpeeds(0, 0);
         }
 
         //decelerate
-        if (_gamepad.leftTrigger.isPressed && thrust > 5)
+        if (_g.leftTrigger.isPressed && thrust > minSpeed)
         {
-            thrust = Mathf.Lerp(thrust, 5, (Time.deltaTime/7));
-            _controllerBody.velocity = Vector3.MoveTowards(_controllerBody.velocity, Vector3.zero, breakForce/5);
+            thrust = Mathf.Lerp(thrust, minSpeed, (Time.deltaTime/3));
+            _controllerBody.velocity = Vector3.MoveTowards(_controllerBody.velocity, Vector3.zero, breakForce/3);
+            _g.SetMotorSpeeds( thrust/maxSpeed * .95f, thrust/maxSpeed *  .26f);
+        }
+        else
+        {
+            _g.SetMotorSpeeds(0, 0);
         }
 
         //twist
-        if (_gamepad.leftStick.left.ReadValue() > .1f)
+        if (_g.leftStick.left.ReadValue() > .1f)
         {
-            _controllerBody.AddRelativeTorque(0,0,(_gamepad.leftStick.left.ReadValue()*3) * (twistSpeed/2));
+            _controllerBody.AddRelativeTorque(0,0,(_g.leftStick.left.ReadValue()*3) * (twistSpeed/2));
         }
-        if (_gamepad.leftStick.right.ReadValue() > .1f)
+        if (_g.leftStick.right.ReadValue() > .1f)
         {
-            _controllerBody.AddRelativeTorque(0,0,-((_gamepad.leftStick.right.ReadValue()*3) * (twistSpeed/2)));
+            _controllerBody.AddRelativeTorque(0,0,-((_g.leftStick.right.ReadValue()*3) * (twistSpeed/2)));
         }
         //pitch
-        if (_gamepad.leftStick.up.isPressed)
+        if (_g.leftStick.up.isPressed)
         {
-            _controllerBody.AddRelativeTorque(_gamepad.leftStick.up.ReadValue() + (downSpeed/(thrust/highSpeedTurning)),0, 0);
+            _controllerBody.AddRelativeTorque(_g.leftStick.up.ReadValue() + (downSpeed/(thrust/highSpeedTurning)),0, 0);
         }
-        if (_gamepad.leftStick.down.isPressed)
+        if (_g.leftStick.down.isPressed)
         {
-            _controllerBody.AddRelativeTorque(-(_gamepad.leftStick.down.ReadValue() + (upSpeed/(thrust/highSpeedTurning))),0, 0);
+            _controllerBody.AddRelativeTorque(-(_g.leftStick.down.ReadValue() + (upSpeed/(thrust/highSpeedTurning))),0, 0);
         }
         //yaw
-        if (_gamepad.leftShoulder.isPressed)
+        if (_g.leftShoulder.isPressed)
         {
             _controllerBody.AddRelativeTorque(0,-yawSpeed,0);
         }
-        if (_gamepad.rightShoulder.isPressed)
+        if (_g.rightShoulder.isPressed)
         {
             _controllerBody.AddRelativeTorque(0,yawSpeed,0);
         }
+        //camera control
+        if (_g.rightStick.y.IsPressed() || _g.rightStick.x.IsPressed())
+        {
+            camera.Follow.localRotation = Quaternion.Euler(80 * _g.rightStick.y.ReadValue(), 80 * _g.rightStick.x.ReadValue(), 0);
+        }
+        else
+        {
+            camera.Follow.localRotation = Quaternion.Euler(Vector3.zero);
+        }
+
+
 
 
     }
