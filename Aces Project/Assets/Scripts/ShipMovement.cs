849be281ca772fad5ef6cdce6395562f2d637584
Connor (Home) <connorcoxr@gmail.com>
2020-11-04 00:43:13 -0700

    Ship gun added
    Former-commit-id: c58ed80cc1303c18f7520914e100de52bbf52868 [formerly a9fc47349890e5d7209f83d9050a1b329873aa63] [formerly d8464452f93522dceac0fd59627511ba4c30438d [formerly c140348710025c94770c15fc28b371f458e994a0]]
    Former-commit-id: 56d3ddd591a02452ee41b6c391a984a6a71c5c21 [formerly 0a6e9edfc6201e643bcc4de4bf95853348dd9337]
    Former-commit-id: 75ade2225184a394703355c8a1951fc7e0d2c189

--- Aces Project/Assets/Scripts/ShipMovement.cs
+++ Aces Project/Assets/Scripts/ShipMovement.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.AccessControl;
+﻿using System.Collections;
+using System.Security.AccessControl;
 using System.Threading;
 using Cinemachine;
 using UnityEngine;
@@ -17,12 +18,21 @@ public class ShipMovement : MonoBehaviour
         breakForce = 1.1f,
         twistSpeed = 1,
         highSpeedTurning = 6,
-        accelerationRate = 5;
+        accelerationRate = 5,
+        missileReload,
+        fireRate;
+
+    public GameObject gunAmmo;
+    public GameObject missile;
+
+    public Transform gunLocation, launcherR, launcherL;
+    
 
     public CinemachineVirtualCamera camera;
 
     private Vector3 _bodyVelocity;
 
+    private bool canFire = true;
     private Rigidbody _controllerBody;
     private Gamepad _g;
     
@@ -31,6 +41,7 @@ public class ShipMovement : MonoBehaviour
        _controllerBody =  GetComponent<Rigidbody>();
        CheckGamepad();
        _g = Gamepad.current;
+       thrust = minSpeed;
     }
 
     private void CheckGamepad()
@@ -78,8 +89,8 @@ public class ShipMovement : MonoBehaviour
         //decelerate
         if (_g.leftTrigger.isPressed && thrust > minSpeed)
         {
-            thrust = Mathf.Lerp(thrust, minSpeed, (Time.deltaTime/3));
-            _controllerBody.velocity = Vector3.MoveTowards(_controllerBody.velocity, Vector3.zero, breakForce/3);
+            thrust = Mathf.Lerp(thrust, minSpeed, (Time.deltaTime));
+            _controllerBody.velocity = Vector3.MoveTowards(_controllerBody.velocity, new Vector3(0,0,thrust), Time.deltaTime*breakForce);
             _g.SetMotorSpeeds( thrust/maxSpeed * .95f, thrust/maxSpeed *  .26f);
         }
         else
@@ -90,20 +101,20 @@ public class ShipMovement : MonoBehaviour
         //twist
         if (_g.leftStick.left.ReadValue() > .1f)
         {
-            _controllerBody.AddRelativeTorque(0,0,(_g.leftStick.left.ReadValue()*3) * (twistSpeed/2));
+            _controllerBody.AddRelativeTorque(0,0,(_g.leftStick.left.ReadValue()*3) * twistSpeed);
         }
         if (_g.leftStick.right.ReadValue() > .1f)
         {
-            _controllerBody.AddRelativeTorque(0,0,-((_g.leftStick.right.ReadValue()*3) * (twistSpeed/2)));
+            _controllerBody.AddRelativeTorque(0,0,-((_g.leftStick.right.ReadValue()*3) * twistSpeed));
         }
         //pitch
         if (_g.leftStick.up.isPressed)
         {
-            _controllerBody.AddRelativeTorque(_g.leftStick.up.ReadValue() + (downSpeed/(thrust/highSpeedTurning)),0, 0);
+            _controllerBody.AddRelativeTorque(_g.leftStick.up.ReadValue() + downSpeed/(thrust/(highSpeedTurning*15)),0, 0);
         }
         if (_g.leftStick.down.isPressed)
         {
-            _controllerBody.AddRelativeTorque(-(_g.leftStick.down.ReadValue() + (upSpeed/(thrust/highSpeedTurning))),0, 0);
+            _controllerBody.AddRelativeTorque(-(_g.leftStick.down.ReadValue() + upSpeed/(thrust/(highSpeedTurning*15))),0, 0);
         }
         //yaw
         if (_g.leftShoulder.isPressed)
@@ -124,8 +135,26 @@ public class ShipMovement : MonoBehaviour
             camera.Follow.localRotation = Quaternion.Euler(Vector3.zero);
         }
 
-
-
-
+        if (_g.buttonSouth.IsPressed() && canFire == true)
+        {
+            StartCoroutine(FireGun());
+        }
+        
+        
     }
+
+
+    private IEnumerator FireGun()
+    {
+        canFire = false;
+        var g = Instantiate(gunAmmo, gunLocation.position, Quaternion.identity);
+        g.GetComponent<Rigidbody>().velocity = transform.forward * 500;
+        yield return new WaitForSeconds(fireRate);
+        canFire = true;
+    }
+
+//    private IEnumerator LaunchMissile()
+//    {
+//        
+//    }
 }
---
+++ Aces Project/Assets/Scripts/projectileBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class projectileBehaviour : MonoBehaviour
+{   
+    public float x, y, z;
+
+    private void Start()
+    {
+        StartCoroutine(TimedDestroy());
+    }
+
+    void OnCollisionEnter()
+    {
+        Destroy(gameObject);
+    }
+
+    private IEnumerator TimedDestroy()
+    {
+
+        yield return new WaitForSeconds(2); Destroy(gameObject);
+
+    }
+}
