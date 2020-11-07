7357fd81f557bcb7124a427afe47bc9bd968933b
Connor (Home) <connorcoxr@gmail.com>
2020-11-07 16:36:21 -0700

    Missile and projectile full functionality
    Former-commit-id: 595135ea85451a9f106c50700fee50f41b277150 [formerly 2f7c1e85ced41b21f900a49843890b1ed8f58de1] [formerly 2cefdbe4ad3b9bccc08f4f24ae27bbaf82139a0b [formerly c9cded1f63cc80fdb089ff99e42043617895d35f]]
    Former-commit-id: 9b69ce53f5eb24d389ed1ef11fcfc980ed2dbaef [formerly 1e4654448dffcdc4fb0ce68a8ed469983e2853c4]
    Former-commit-id: b02074c3066513c9e6cd429ccdebc02dc69fd700

---
+++ Aces Project/Assets/Scripts/MissileBehaviour.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class MissileBehaviour : MonoBehaviour
+{    
+    public int destroyTime = 10;
+    public int missileSpeed;
+    public GameObject explosion;
+    
+   
+    private Transform _target;
+    private Rigidbody _rb;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+        StartCoroutine(TimedDestroy());
+        
+    }
+
+    private void FixedUpdate()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(gameObject.transform.position, Vector3.forward, out hit, 40, 10))
+        {
+
+            _target = hit.collider.transform;
+        }
+        if(_target != null)
+        _rb.transform.LookAt(_target);
+        _rb.GetComponent<Rigidbody>().AddRelativeForce(0, 0, 80 * missileSpeed);
+    }
+
+    void OnCollisionEnter()
+    {
+        Destroy(gameObject);
+        Instantiate(explosion, transform.position, Quaternion.identity);
+    }
+
+    private IEnumerator TimedDestroy()
+    {
+
+        yield return new WaitForSeconds(destroyTime); Destroy(gameObject);
+
+    }
+}
---
+++ Aces Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    protected Rigidbody _controllerBody;
+    protected Gamepad _g;
+
+    protected void Start()
+    {
+        _controllerBody = GetComponent<Rigidbody>();
+        CheckGamepad();
+    }
+    
+    protected void CheckGamepad()
+    {
+        _g = Gamepad.current;
+    }
+}
