5e7a57e6ddea9a7da5f4c08f4e8b4e34295f39a9
Connor Cox <connorcoxr@gmail.com>
2021-02-20 22:50:53 -0700

    Clean Up, Layouts, Ship Update
    Former-commit-id: 377b9a96270b776c970ed79b4150e6b240bc7150 [formerly ebf48ce28e5f80e6adabf933c09cf0712213a135] [formerly f6b677357a72b40de6148faa4a1febdebaf6fda7 [formerly 991693b7b32174aa131c49f7f239c1ec5bda4807]]
    Former-commit-id: 13d4bded448937766e0c0bcc98b3ae8d9364a864 [formerly bc55bd2990790921f5643231a41423bc5bd3dcc8]
    Former-commit-id: 24f7454b9a40f5a6decec25b74218c87947413a5

---
+++ Project Aces URP/Assets/Controller/Scripts/AircraftInputs.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class AircraftInputs : MonoBehaviour, ControlInputActions.IFlightActions
+{
+    public AircraftData aircraftData;
+    
+    private float thrustInput, yawInput, brakeInput;
+    private Vector2 torqueInput, cameraInput;
+    private Rigidbody _rb;
+    private ControlInputActions controls;
+    
+    public void OnEnable()
+    {
+        if (controls == null)
+        {
+            controls = new ControlInputActions();
+            // Tell the "gameplay" action map that we want to get told about
+            // when actions get triggered.
+            controls.Flight.SetCallbacks(this);
+        }
+        controls.Flight.Enable();
+    }
+
+    private void Update()
+    {
+        SendInputData();
+    }
+    
+    public void OnMenuButton(InputAction.CallbackContext pressed)
+    {
+        print("Menu button pressed");
+    }
+
+    public void OnThrust(InputAction.CallbackContext value)
+    {
+        thrustInput = value.ReadValue<float>();
+
+    }
+
+    public void OnBrake(InputAction.CallbackContext value)
+    {
+        brakeInput = value.ReadValue<float>();
+
+    }
+
+    public void OnYaw(InputAction.CallbackContext value)
+    {
+        yawInput = value.ReadValue<float>();
+    }
+
+    public void OnTorque(InputAction.CallbackContext value)
+    {
+        torqueInput = value.ReadValue<Vector2>();
+    }
+    
+    private void SendInputData()
+    {
+        aircraftData.UpdateInputData(cameraInput, torqueInput, thrustInput, yawInput, brakeInput);
+    }
+}
---
+++ Project Aces URP/Assets/Scripts/FloatData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "FloatData", menuName = "Project Aces URP/FloatData", order = 0)]
+public class FloatData : ScriptableObject 
+{
+    public float value;
+
+}
