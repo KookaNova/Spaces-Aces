d94aa792d85e8e07e2741c38b9dc8ab6c4603147
Connor Cox <connorcoxr@gmail.com>
2021-02-21 01:37:12 -0700

    Flight Controls, UI additions

---
+++ Project Aces URP/Assets/Scripts/DataFillAdjustBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DataFillAdjustBehaviour : MonoBehaviour
+{
+    public FloatData fillData;
+    public Image fillImage;
+
+    public float maxValue;
+    private float _currentValue;
+    
+    void Start()
+    {
+        fillImage = GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        _currentValue = fillData.value / maxValue;
+        fillImage.fillAmount = _currentValue;
+    }
+}
