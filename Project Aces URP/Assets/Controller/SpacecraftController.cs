b7364c91474b1d444f881750ac15e8558aa7e31e
Connor Cox <connorcoxr@gmail.com>
2021-05-02 02:42:23 -0600

    Gun control refined

--- Project Aces URP/Assets/Controller/SpacecraftController.cs
+++ Project Aces URP/Assets/Controller/SpacecraftController.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
@@ -8,10 +7,10 @@ using UnityEngine.UI;
 public class SpacecraftController : MonoBehaviour, ControlInputActions.IFlightActions
 {
     [Header("Spacecraft Objects")]
-    public GameObject shipObject;
-    public GameObject explosionObject, gunAmmoObject, missileObject;
-    public Image gunReticle, aimRange;
-    public Camera playerCamera;
+    public GameObject firstCam;
+    public GameObject shipObject, explosionObject, gunAmmoObject, missileObject, lockIndicator;
+    public Canvas hudCanvas;
+    public Image aimReticle;
     public Transform[] gunPosition, missilePosition;
     public Transform[] targetObj;
 
@@ -25,32 +24,34 @@ public class SpacecraftController : MonoBehaviour, ControlInputActions.IFlightAc
         pitch = 7, 
         yaw = 3,
         fireRate = 0.5f,
-        gunSpeed = 1000;
+        gunSpeed = 1000,
+        gunSensitivity = 1,
+        gunRange = 3,
+        lockDistance = 3000;
     //player inputs 
     private float thrustInput, yawInput, brakeInput;
-    private Vector2 torqueInput, cameraInput, screenOut, mousePosition;
+    private Vector2 torqueInput, cameraInput, aimReticlePosition;
+    //Utility Inputs
     private bool canFire = true, gunIsFiring = false;
-
     private Rigidbody _rb;
     private ControlInputActions _controls;
 
     //Input System Setup-----------------------------------------------
     private void Awake(){
         _rb = GetComponent<Rigidbody>();
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Confined;
     }
     private void OnEnable() {
         _controls = new ControlInputActions();
         _controls.Flight.SetCallbacks(this);
         _controls.Flight.Enable();
+        Cursor.visible = false;
     }
     private void OnDisable() {
         _controls.Flight.Disable();
     }
     //Collision---------------------------------------------------------
     private void OnCollisionEnter(Collision collision) {
-        if(collision.gameObject.layer == LayerMask.NameToLayer("Crash Hazard")){
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Crash Hazard") || collision.gameObject.layer == LayerMask.NameToLayer("Enemy Player")){
             print("crash");
 
            var deathLocation = transform.position;
@@ -61,10 +62,13 @@ public class SpacecraftController : MonoBehaviour, ControlInputActions.IFlightAc
     }
     //Input Actions-----------------------------------------------------
     
+    public void OnMenuButton(InputAction.CallbackContext context){}
+    public void OnCameraChange(InputAction.CallbackContext context){
+        firstCam.SetActive(context.ReadValueAsButton());
+    }
     public void OnBrake(InputAction.CallbackContext value){
         brakeInput = value.ReadValue<float>();
     }
-    public void OnMenuButton(InputAction.CallbackContext context){}
     public void OnThrust(InputAction.CallbackContext value){
         thrustInput = value.ReadValue<float>();
     }
@@ -75,7 +79,7 @@ public class SpacecraftController : MonoBehaviour, ControlInputActions.IFlightAc
         yawInput = value.ReadValue<float>();
     }
      public void OnAimGun(InputAction.CallbackContext position){
-        mousePosition = position.ReadValue<Vector2>();
+        aimReticlePosition = position.ReadValue<Vector2>();
     }
     public void OnGunFire(InputAction.CallbackContext pressed){
         gunIsFiring = pressed.ReadValueAsButton();
@@ -84,60 +88,57 @@ public class SpacecraftController : MonoBehaviour, ControlInputActions.IFlightAc
     }
 
     //Player is controlled-----------------------------------------------
-    private void FixedUpdate() 
-    {
+    private void FixedUpdate() {
         ThrustControl();
         TorqueControl();
-        
-        Ray ray = Camera.main.ScreenPointToRay(gunReticle.transform.position);
-        
-        gunReticle.transform.position = mousePosition;
-        for(int i = 0; i < gunPosition.Length; i++){
-            gunPosition[i].LookAt(ray.GetPoint(10000f));
-        }
-
+        GunControl();
 
         _rb.AddRelativeForce(0,0,currentSpeed.value);
 
-        //shift speed to certain defaults
-        if (gameObject.transform.localRotation.x < -60 && gameObject.transform.localRotation.x > -135)
-        {
-            currentSpeed.value = Mathf.Lerp(currentSpeed.value, minSpeed, .1f);
-        }
-        
         if (currentSpeed.value > cruiseSpeed)
         {
             currentSpeed.value = Mathf.Lerp(currentSpeed.value, cruiseSpeed, .001f);
         }
-        
         currentSpeed.value = Mathf.Lerp(currentSpeed.value, minSpeed, brakeInput * .01f);
     }
 
-    private void ThrustControl()
-    {
+    private void ThrustControl(){
         var speed = currentSpeed.value + (thrustInput * acceleration);
-        currentSpeed.value = Mathf.Lerp(currentSpeed.value, speed, .005f);
+        currentSpeed.value = Mathf.Lerp(currentSpeed.value, speed, Time.deltaTime);
         currentSpeed.value = Mathf.Clamp(currentSpeed.value, minSpeed, maxSpeed);
-
     }
 
-    private void TorqueControl()
-    {
+    private void TorqueControl(){
          var highspeedhandling = currentSpeed.value/maxSpeed + 1;
-        Vector3 torque  = new Vector3((torqueInput.y * pitch) / highspeedhandling, yawInput * yaw, (torqueInput.x * roll) / highspeedhandling);
-        _rb.AddRelativeTorque(torque);
+        Vector3 torqueForce  = new Vector3((torqueInput.y * pitch) / highspeedhandling, yawInput * yaw, (torqueInput.x * roll) / highspeedhandling);
+        _rb.AddRelativeTorque(torqueForce);
     }
 
     private void Targeting(){
-        
-
         int currentTarget = 0;
         if(currentTarget > targetObj.Length){
             currentTarget = 0;
         }
-        aimRange.transform.position = playerCamera.WorldToScreenPoint(targetObj[currentTarget].position);
+        lockIndicator.transform.position = Camera.main.WorldToScreenPoint(targetObj[currentTarget].position);
+    }
 
-        
+    private void GunControl(){
+        //limit reticle position and speed
+        Vector2 reticleDirection = new Vector2();
+        var viewWidth = Camera.main.scaledPixelWidth;
+        var viewHeight = Camera.main.scaledPixelHeight;
+        reticleDirection = new Vector2(Mathf.Lerp(aimReticle.transform.position.x, aimReticlePosition.x, gunSensitivity*Time.deltaTime),
+            Mathf.Lerp(aimReticle.transform.position.y, aimReticlePosition.y, gunSensitivity*Time.deltaTime));
+        var x = Mathf.Clamp(reticleDirection.x, viewWidth/gunRange, viewWidth - viewWidth/gunRange);
+        var y = Mathf.Clamp(reticleDirection.y, viewHeight/gunRange, viewHeight - viewHeight/gunRange);
+        reticleDirection = new Vector2(x,y);
+
+        aimReticle.transform.position = reticleDirection;
+        //aim gun position towards reticle
+        Ray ray = Camera.main.ScreenPointToRay(aimReticle.transform.position);
+        for(int i = 0; i < gunPosition.Length; i++){
+            gunPosition[i].LookAt(ray.GetPoint(10000f));
+        }
     }
 
     IEnumerator FireGun(){
@@ -147,8 +148,7 @@ public class SpacecraftController : MonoBehaviour, ControlInputActions.IFlightAc
                 var g = Instantiate(gunAmmoObject);
                 g.transform.position = gunPosition[i].position;
                 g.transform.rotation = gunPosition[i].rotation;
-                g.GetComponent<Rigidbody>().velocity = gunPosition[i].transform.forward * gunSpeed;
-                print("fire " + i);
+                g.GetComponent<Rigidbody>().velocity = gunPosition[i].transform.forward *(currentSpeed.value + gunSpeed);
                 yield return new WaitForSeconds(fireRate);
             }
         }
---
+++ Project Aces URP/Assets/Prefabs/TargetData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetData : MonoBehaviour
+{
+    public List<GameObject> global;
+
+    public void AddGlobalTargets(GameObject obj){
+        global.Add(obj);
+    }
+}
