372423f882e480982dc3e34b0d61052794b95b65
Connor Cox <connorcoxr@gmail.com>
2021-07-09 11:49:53 -0600

    Logo added to start screen

---
+++ Project Aces URP/Assets/Scripts/ArtificialPlayerController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtificialPlayerController : MonoBehaviour
+{
+    public enum DecisionStates{
+        RandomFlying,
+    }
+}
