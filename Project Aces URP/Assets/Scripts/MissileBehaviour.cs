e2541ec3896fcb37060e978ba6133d90ed9e956f
Connor Cox <connorcoxr@gmail.com>
2021-07-19 21:07:03 -0600

    Basic radar tracking, improved missile accuracy
    Former-commit-id: 7f61ba04b03641fab6d073e13faa56a876822cb0 [formerly f1b5c39f740580901f6eb84ff28d2835ebfa2919] [formerly 0fa9f95bb45235764250dc4920b2c8e206aa56f4 [formerly 089af222000204a146cdf00858f19b099e5360e5]]
    Former-commit-id: 2e26f944ed7bcd2fc41a786ebea5a3e063af381b [formerly e0df5da10f6463da58144a533c4b38efe15121f2]
    Former-commit-id: 7f82916b7b9a864abdb95d0ed54c4c125bad1ffc

--- Project Aces URP/Assets/Scripts/MissileBehaviour.cs
+++ Project Aces URP/Assets/Scripts/MissileBehaviour.cs
@@ -10,13 +10,14 @@ public class MissileBehaviour : MonoBehaviour
     public GameObject target = null;
     private Rigidbody _rb;
     private Collider _col;
+    private bool startCasting = false;
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody>();
         _col = GetComponent<Collider>();
         _col.enabled = false;
-        _rb.AddRelativeForce(0,0,currentSpeed + 300, ForceMode.Impulse);
+        _rb.AddRelativeForce(0,0,currentSpeed + 300, ForceMode.VelocityChange);
 
         StartCoroutine(Missile());
         StartCoroutine(WakeCollider());
@@ -28,17 +29,24 @@ public class MissileBehaviour : MonoBehaviour
         
         if(target != null)
         {
-            if(Physics.SphereCast(gameObject.transform.position, 5, gameObject.transform.forward, out hit)){
-                if(hit.collider.gameObject != target.gameObject){
+            if(startCasting == true){
+                if(Physics.SphereCast(gameObject.transform.position, 75, gameObject.transform.forward, out hit)){
+                    if(hit.collider.gameObject != target.gameObject){
+                        target = null;
+                        return;
+                    }
+                }
+                else{
                     target = null;
                     return;
                 }
             }
+
             print(target);
             var toTarget = target.transform.position - _rb.position;
             var targetRotation = Quaternion.LookRotation(toTarget);
             
-            _rb.rotation = Quaternion.RotateTowards(_rb.rotation, targetRotation, seeking);
+            _rb.rotation = Quaternion.RotateTowards(_rb.rotation, targetRotation, seeking * 1000 * Time.deltaTime);
         }
         else{
             _rb.rotation = _rb.rotation;
@@ -57,6 +65,7 @@ public class MissileBehaviour : MonoBehaviour
     private IEnumerator WakeCollider(){
         yield return new WaitForSeconds(.5f);
         _col.enabled = true;
+        startCasting = true;
         
     }
 
