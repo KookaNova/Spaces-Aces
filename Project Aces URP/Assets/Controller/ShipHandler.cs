c9a4357ff445d85c0666a74064495131dcac0620
Connor Cox <connorcoxr@gmail.com>
2021-07-28 12:59:22 -0600

    Multiplayer test menu access, ship data added
    Former-commit-id: 74024be2f81656f8877e9af2f5ed335f9213dc0c [formerly 06ba201e48d479d7e6df6c7f75f3750d84bbfd89] [formerly 3a993b8b2cceffba0d1151c726c312ea7eb037cd [formerly 6b9d7c6abb32529ca00f759bd27fe27ecc23b75e]]
    Former-commit-id: 4e6bf68aa179eba1a189a180a5a3727bb14ae184 [formerly b5e9c6305882a89eef5a9364fb40e85f324f209b]
    Former-commit-id: 75634a08fd30718fe8bd7dd136e005c1dd70537b

---
+++ Project Aces URP/Assets/Controller/ShipHandler.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "Controller/Ship")]
+public class ShipHandler : ScriptableObject
+{
+
+}
