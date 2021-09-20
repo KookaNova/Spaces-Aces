9ebbf07592fd9acddeb9372de447bdebd9b7726b
Connor Cox <46508202+ConnorCoxAGD@users.noreply.github.com>
2021-09-19 22:33:05 -0600

    Character select scripts and scene created
    Former-commit-id: bb7eb9673fe83e4431a1c15992abf71828a56560 [formerly fe124c1c3d9e49bc0f5fcaa06348163c6557ea0b] [formerly ef8a4916e6998c028a77639954936bee61758502 [formerly b28d53e4f0626509ac8c7c09178ecac13f084b7e]]
    Former-commit-id: 65eeb12c7d177d2e5a5b27e7fdb8eff06383784c [formerly 2f2bf9f113104b94944a76df272b457f5fda667b]
    Former-commit-id: 3d4691cefc03e1a317640e689c327eed047963a0

---
+++ Project Aces URP/Assets/Characters/CharacterHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Controller/Character")]
+public class CharacterHandler : ScriptableObject
+{
+    public string characterName = "Character Name";
+    [TextArea( 2, 50 )]
+    public string bio = "Character bio...";
+    public Sprite portrait, bodyArt, nameArt;
+    [Tooltip("Abilities are ordered 0 = Passive, 1 = Primary, 2 = Secondary, and 3 = Ace")]
+    public List<AbilityHandler> abilities;
+}
