d8c518513819648f9be459b7bde506cd1a2868f3
Connor Cox <46508202+ConnorCoxAGD@users.noreply.github.com>
2021-09-30 13:52:15 -0600

    Added virtual methods, updated documentation

--- Project Aces URP/Assets/Characters/AbilityHandler.cs
+++ Project Aces URP/Assets/Characters/AbilityHandler.cs
@@ -2,14 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AbilityHandler : ScriptableObject
+///<summary> Abilities inherit from Ability Handler. The ability handler holds virtual functions which the SpaceCraft Controller calls to activate abilities.
+/// The Spacecraft controller can Start Activate Coroutines, things like second activate, and can run certain parts on Update while a bool is checked.
+/// Ability Handler inherits from Scriptable Object to make it easy to apply to a character's ability list without creating needless prefabs or another more
+/// complex form of data object.
+///</summary>
+public abstract class AbilityHandler : ScriptableObject
 {
     public string abilityName;
     public Sprite icon;
     public float cooldownTime = 3f, startUpTime = .2f;
-
     public ParticleSystem startUpParticle, endParticle;
-
     public GameObject player;
+
+    public bool canUse = true;
+
+    [HideInInspector] public bool isActive = false, isUpdating = false;
+    
+
+    #region Virtual Functions
+    ///<summary> Call with StartCoroutine(Activate()). Abilities override this function to perform a specific action on a button press. </summary>
+    public virtual IEnumerator Activate(){
+        yield break;
+    }
+
+    ///<summary> Call in Update(), FixedUpdate(), or LateUpdate(). Should be called with the check "if(isUpdating)" or "while(isUpdating)."
+    /// Abilities override the function and perform a specific function on Update() when called correctly. </summary>
+    public virtual void RunOnUpdate() {}
+
+    #endregion
 
 }
