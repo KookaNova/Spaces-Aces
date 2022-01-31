f216a58d65dffe1ec2171e1b8d4b29635d55f2cc
Connor Cox <46508202+ConnorCoxAGD@users.noreply.github.com>
2022-01-30 18:05:46 -0700

    PlayerPrefs and User Settings

--- Project Aces URP/Assets/Scripts/EditPreferences.cs
+++ Project Aces URP/Assets/Scripts/EditPreferences.cs
@@ -1,21 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 using UnityEngine.Audio;
 
 static class EditPreferences
 {
-    //graphics
-    [Range(0,100)]
-    public static float brightness = 50; //0-100 or 0-1
     public static int
         displayMode = 0,   //0=Exclusive Fullscreen, 1=FullScreen Window, 2=Maximized Window, 3=Windowed
         resolution = 4,    //0=2560x1440, 1=1920x1080, 2=1600x900, 3=1366x768, 4=1280x720, 5=864x486
-        quality = 0,       //0=High, 1=Medium, 2=Low
+        quality = 2,       //0=Low, 1=Medium, 2=High
         textureRes = 0,    //0=Full, 1= Half, 2=Quarter
-        aa = 0,            //anti-aliasing: 0=Fast (FXAA), 1=Fancy (SMAA)
-        shadowQuality = 0; //0=High,
-
+        aa = 2,            //None, 2x, 4x
+        shadowQuality = 0; //0=low, medium, high, veryhigh
+    public static bool 
+        usingRealtimeReflections = true,
+        usingBetterParticles = true,
+        usingVSync = false;
     //audio
     public static AudioMixer finalMix = Resources.Load<AudioMixer>("Audio/Mixers/FinalMix");
     [Range(-80, 20)]
@@ -34,6 +35,7 @@ static class EditPreferences
         finalMix.SetFloat("v_Env", environment);
         finalMix.SetFloat("v_Dialogue", dialogue);
     }
+
 
     public static void UpdateGraphicsSettings(){
         FullScreenMode fsm;
@@ -60,6 +62,7 @@ static class EditPreferences
                 fsm = FullScreenMode.ExclusiveFullScreen;
                 break;
         }
+        //Resolution
         switch (resolution){
             case 0:
                 Screen.SetResolution(2560, 1440, fsm);
@@ -82,11 +85,86 @@ static class EditPreferences
             default:
                 Screen.SetResolution(1280, 720, fsm);
                 break;
-
         }
-
-
+        //Quality Mode
+        QualitySettings.SetQualityLevel(quality);
+        QualitySettings.masterTextureLimit = textureRes;
+        //Anti-Aliasing None, 2x, 4x
+        switch (aa){
+            case 0:
+                QualitySettings.antiAliasing = 0;
+                break;
+            case 1:
+                QualitySettings.antiAliasing = 2;
+                break;
+            case 2:
+                QualitySettings.antiAliasing = 4;
+                break;
+        }
         
+        switch (shadowQuality){
+            case 0:
+                QualitySettings.shadowResolution = ShadowResolution.Low;
+                break;
+            case 1:
+                QualitySettings.shadowResolution = ShadowResolution.Medium;
+                break;
+            case 2:
+                QualitySettings.shadowResolution = ShadowResolution.High;
+                break;
+            case 3:
+                QualitySettings.shadowResolution = ShadowResolution.VeryHigh;
+                break;
+        }
+        QualitySettings.realtimeReflectionProbes = usingRealtimeReflections;
+        QualitySettings.softParticles = usingBetterParticles;
+        if(usingVSync){
+            QualitySettings.vSyncCount = 1;
+        }
+        else{
+            QualitySettings.vSyncCount = 0;
+        }
         
     }
+
+    public static void LoadSettings(){
+        displayMode = PlayerPrefs.GetInt("DisplayMode");
+        resolution = PlayerPrefs.GetInt("Resolution");
+        quality = PlayerPrefs.GetInt("Quality");
+        textureRes = PlayerPrefs.GetInt("TextureRes");
+        aa = PlayerPrefs.GetInt("Anti-Aliasing");
+        shadowQuality = PlayerPrefs.GetInt("ShadowQuality");
+        usingRealtimeReflections = PlayerPrefs.GetInt("RealtimeReflections") == 1;
+        usingBetterParticles = PlayerPrefs.GetInt("BetterParticles") == 1;
+        usingVSync = PlayerPrefs.GetInt("VSync") == 1;
+        master = PlayerPrefs.GetFloat("Master");
+        music = PlayerPrefs.GetFloat("Music");
+        sound = PlayerPrefs.GetFloat("Sound");
+        environment = PlayerPrefs.GetFloat("Environment");
+        dialogue = PlayerPrefs.GetFloat("Dialogue");
+
+        UpdateAudioSettings();
+        UpdateGraphicsSettings();
+    }
+
+    public static void SaveSettings(){
+        PlayerPrefs.SetInt("DisplayMode", displayMode);
+        PlayerPrefs.SetInt("Resolution", resolution);
+        PlayerPrefs.SetInt("Quality", quality);
+        PlayerPrefs.SetInt("TextureRes", textureRes);
+        PlayerPrefs.SetInt("Anti-Aliasing", aa);
+        PlayerPrefs.SetInt("ShadowQuality", shadowQuality);
+        PlayerPrefs.SetInt("RealtimeReflections", usingRealtimeReflections? 1:0);
+        PlayerPrefs.SetInt("BetterParticles", usingBetterParticles? 1:0);
+        PlayerPrefs.SetInt("VSync", usingVSync? 1:0);
+
+        PlayerPrefs.SetFloat("Master", master);
+        PlayerPrefs.SetFloat("Music", music);
+        PlayerPrefs.SetFloat("Sound", sound);
+        PlayerPrefs.SetFloat("Environment", environment);
+        PlayerPrefs.SetFloat("Dialogue", dialogue);
+        PlayerPrefs.Save();
+    }
 }
+
+
