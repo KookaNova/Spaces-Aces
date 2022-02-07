f29e91cb512eaf876852acba58166f55f39acdfa
Connor Cox <46508202+ConnorCoxAGD@users.noreply.github.com>
2022-02-07 12:31:08 -0700

    Fixed lighting warnings, fixed ship collision

--- Project Aces URP/Assets/ShipBehaviour.cs
+++ Project Aces URP/Assets/ShipBehaviour.cs
@@ -5,6 +5,18 @@ using Cox.PlayerControls;
 
 public class ShipBehaviour : MonoBehaviour
 {
-    public SpacecraftController sc;
+    SpacecraftController sc;
+
+    public void SetController(SpacecraftController newController){
+        sc = newController;
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        //On collision with hazards or other players, damage the player, based partially on speed.
+        print("Collision");
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Crash Hazard") || collision.gameObject.layer == LayerMask.NameToLayer("Player")){
+           sc.TakeDamage(sc.currentSpeed * 8, null, "accident");
+        }
+    }
     
 }
