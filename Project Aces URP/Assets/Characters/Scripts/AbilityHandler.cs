7d18ba098d1155c6715e9415a112c2efebbd49f7
Connor Cox <46508202+ConnorCoxAGD@users.noreply.github.com>
2022-02-12 02:51:39 -0700

    Added ability icons and auto loading them into the UI

--- Project Aces URP/Assets/Characters/Scripts/AbilityHandler.cs
+++ Project Aces URP/Assets/Characters/Scripts/AbilityHandler.cs
@@ -13,7 +13,7 @@ public abstract class AbilityHandler : ScriptableObject
     public string abilityName;
     [TextArea]
     public string abilityDescription;
-    public Sprite icon;
+    public Texture2D icon;
     public float cooldownTime = 3f, startUpTime = .2f;
     public GameObject startUpEffect, endEffect;
 
