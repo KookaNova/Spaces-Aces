c91d4517ab01937816cae2dc826974db63dcb6b6
Connor Cox <46508202+ConnorCoxAGD@users.noreply.github.com>
2022-03-23 11:15:21 -0600

    Weapon scripts moved to folder for organization

---
+++ Project Aces URP/Assets/Scripts/Weaponry/GunAmmoBehaviour.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+using Photon.Pun;
+
+namespace Cox.PlayerControls{
+public abstract class GunAmmoBehaviour : MonoBehaviourPun
+{
+    [HideInInspector] public SpacecraftController owner = null;
+
+    [SerializeField] float destroyTime = 6, colliderDelay = .1f;
+    
+    public float damageOutput = 223, speed = 1000;
+    public GameObject impactObj;
+
+    private Collider thisCollider;
+
+    private void Awake() {
+        this.GetComponent<Rigidbody>().velocity = this.transform.forward * speed;
+        thisCollider = GetComponent<Collider>();
+        thisCollider.enabled = false;
+        StartCoroutine(StartUp());
+        
+    }
+
+    public void OnCollisionEnter(Collision obj) {
+        if(impactObj != null){
+            var impact = Instantiate(impactObj);
+            impact.transform.position = transform.position;
+        }
+        
+        if(obj.gameObject.GetComponentInParent<SpacecraftController>()){
+            obj.gameObject.GetComponentInParent<SpacecraftController>().TakeDamage(damageOutput, owner, "gun");
+            owner.TargetHit();
+        }
+        if(photonView.IsMine) PhotonNetwork.Destroy(this.gameObject);
+    }
+
+    private IEnumerator StartUp(){
+        yield return new WaitForSeconds(colliderDelay);
+        thisCollider.enabled = true;
+
+        yield return new WaitForSeconds(destroyTime);
+        PhotonNetwork.Destroy(this.gameObject);
+    }
+}
+}
---
+++ Project Aces URP/Assets/Scripts/Weaponry/MissileBehaviour.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using UnityEngine;
+using Photon.Pun;
+
+namespace Cox.PlayerControls{
+
+[RequireComponent(typeof(Rigidbody), typeof(Collider))]
+public class MissileBehaviour : MonoBehaviourPun
+{
+    public int destroyTime = 10;
+    public float missileSpeed = 800, turningLimit = 20, missProbability = 2, currentSpeed, damageOutput = 1000;
+
+    public GameObject explosion;
+    [HideInInspector]public SpacecraftController owner = null;
+    [HideInInspector]public GameObject target = null;
+    [HideInInspector]public Rigidbody rb;
+    private Collider _col;
+    private bool startCasting = false;
+    public TrailRenderer trail;
+    public bool isEmitting = true;
+
+    private SpacecraftController sc;
+
+    [HideInInspector]public bool missileHit = false, missileMissed = false;
+
+    private void Awake()
+    {
+        trail = GetComponentInChildren<TrailRenderer>();
+        rb = GetComponent<Rigidbody>();
+        _col = GetComponent<Collider>();
+        _col.enabled = false;
+        rb.velocity = transform.forward * (currentSpeed + missileSpeed);
+
+        StartCoroutine(Missile());
+        StartCoroutine(WakeCollider());
+        
+    }
+
+    private void Update()
+    {
+        int layer = 1 << 14;
+
+        Debug.DrawLine(gameObject.transform.position, gameObject.transform.position + (gameObject.transform.forward * 2500), Color.red);
+        RaycastHit hit;
+        
+        if(target != null){
+            if(target.GetComponentInParent<SpacecraftController>()){
+               sc = target.GetComponentInParent<SpacecraftController>();
+               sc.missileChasing = true;
+            }
+            if(startCasting == true){
+                if(Physics.SphereCast(gameObject.transform.position, missProbability, gameObject.transform.forward, out hit, 5000, ~layer)){
+                    if(hit.rigidbody == null || hit.rigidbody.gameObject != target){
+                        target = null;
+                        missileMissed = true;
+                        sc.missileChasing = false;
+                        sc.missileClose = false;
+                        
+                        Debug.Log("MissileBehaviour: Spherecast obstructed. Missile missed.");
+                        if(hit.rigidbody == null){
+                            Debug.Log("Missile: Did not hit a RigidBody");
+                            return;
+                        }
+                        
+                        return;
+                    }
+                    if(hit.distance < 600){
+                        sc.missileClose = true;
+                    }
+                    else{
+                        sc.missileClose = false;
+                    }
+                }
+                else{
+                    target = null;
+                    missileMissed = true;
+                    sc.missileChasing = false;
+                    sc.missileClose = false;
+
+                    Debug.Log("MissileBehaviour: Spherecast missed the target. Missile missed. No hit.");
+                    return;
+                }
+            }
+            
+            var toTarget = target.transform.position - rb.position;
+            var targetRotation = Vector3.RotateTowards(rb.transform.forward, toTarget, turningLimit * Time.fixedDeltaTime, 1080);
+            //var step = turningLimit * Time.fixedDeltaTime;
+
+            //_rb.rotation = targetRotation;
+            rb.transform.rotation = Quaternion.LookRotation(targetRotation);
+        }
+        else{
+            rb.rotation = rb.rotation;
+        }
+        
+    }
+
+    private void FixedUpdate() {
+        rb.AddRelativeForce(0,0,currentSpeed + missileSpeed, ForceMode.Acceleration);
+        if(isEmitting){
+            trail.emitting = true;
+        }
+    }
+
+
+    private void OnCollisionEnter(Collision obj)
+    {
+        if(obj.gameObject.GetComponentInParent<SpacecraftController>()){
+            obj.gameObject.GetComponentInParent<SpacecraftController>().TakeDamage(damageOutput, owner, "missile");
+            if(owner.isActiveAndEnabled){
+                owner.TargetHit();
+            }
+            
+            if(sc != null){
+                sc.missileChasing = false;
+            }
+        }
+        EndUse();
+    }
+
+    public void EndUse(){
+        Instantiate(explosion, transform.position, Quaternion.identity);
+        trail.transform.parent = null;
+        if(photonView.IsMine) PhotonNetwork.Destroy(gameObject);
+    }
+
+
+    private IEnumerator WakeCollider(){
+        yield return new WaitForSeconds(.1f);
+        _col.enabled = true;
+        startCasting = true;
+        
+    }
+
+    private IEnumerator Missile()
+    {
+        if (target != null)
+        {
+            rb.transform.LookAt(target.transform);
+        }
+        yield return new WaitForSecondsRealtime(destroyTime); 
+        trail.transform.parent = null; PhotonNetwork.Destroy(gameObject);
+
+    }
+}
+}
